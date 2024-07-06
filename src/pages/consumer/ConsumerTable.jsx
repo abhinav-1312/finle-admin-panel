ccb266b9aeb9d02cb8b8b2883e503e157da4489b
abhinav-1312 <abhinav@dkgrouplabs.com>
2024-07-06 08:49:15 +0530

    issue

--- src/pages/consumer/ConsumerTable.jsx
+++ src/pages/consumer/ConsumerTable.jsx
@@ -8,13 +8,29 @@ sharing or distribution without prior written consent from the copyright holder<
 import React, { useEffect, useState } from "react";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { BASE_URL, TOKEN } from "../../utils/BaseUrl";
-import { Button, Card, CardContent, TextField } from "@mui/material";
+import { Box, Button, Card, CardContent, IconButton, Modal, TextField, Typography } from "@mui/material";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { Edit } from "@mui/icons-material";
+
+const style = {
+  position: "absolute",
+  top: "40%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 1000,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+};
 
 const ConsumerTable = () => {
   const [users, setUsers] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [modalOpen, setModalOpen] = useState(false)
+  const [consumerFormData, setConsumerFormData] = useState(null)
 
   const handleSearch = (e) => {
     setSearchTerm(e.target.value);
@@ -51,6 +67,14 @@ const ConsumerTable = () => {
     navigate(`/consumer/${userId}`)
   }
 
+  const editConsumer = (obj) => {
+    console.log("Obj: ", obj)
+    setConsumerFormData({...obj})
+    setModalOpen(true)
+  }
+
+  console.log("cfd: ", consumerFormData) 
+
   const filteredUsers = searchTerm
     ? users.filter((user) =>
       Object.values(user).some(
@@ -81,17 +105,44 @@ const ConsumerTable = () => {
      },
     { field: "firstName", headerName: "First Name", width: 150 },
     { field: "lastName", headerName: "Last Name", width: 150 },
-    // { field: "userType", headerName: "User Type", width: 150 },
     { field: "mobileNumber", headerName: "Mobile Number", width: 180 },
-    // { field: "userMode", headerName: "User Mode", width: 120 },
-    // { field: "createdBy", headerName: "Created By", width: 150 },
-    // { field: "createdDate", headerName: "Created Date", width: 180 },
     { field: "active", headerName: "Active", width: 120 },
+    { field: "action", headerName: "Action", width: 120,
+      renderCell: (params) => (
+        <IconButton onClick={() => editConsumer(params.row)}>
+            <Edit />
+          </IconButton>
+      )
+     },
     // { field: "adminFlag", headerName: "Admin Flag", width: 120 },
   ];
 
+  const handleModalClose = () => {
+    setModalOpen(false)
+  }
+
+  const handleFormChange = (event) => {
+    const { name, value } = event.target;
+    setConsumerFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault()
+    const url = "https://finle-user-service.azurewebsites.net/user-service/updateUser"
+    try{
+      const res = await axios.post(url, consumerFormData)
+      alert("Consumer data editted successfully!")
+    }catch(error){
+      console.log("Error on edit.", error)
+      alert("Error on editing consumer. Please try again!")
+    }
+    finally{
+      setModalOpen(false)
+    }
+  }
+
   return (
-    <>
+    <div>
       <h1>Consumer Details</h1>
 
       <Card>
@@ -129,7 +180,49 @@ const ConsumerTable = () => {
           </div>
         </CardContent>
       </Card>
-    </>
+
+      <Modal open={modalOpen} onClose={handleModalClose}> 
+        <Box sx={{...style}}>
+          <Typography variant="h6">Edit Consumer</Typography>
+
+          <form onSubmit={handleFormSubmit} style={{display: "grid", gridTemplateColumns: "repeat(auto-fit(min-max(220px, 1fr)))", gap: "1rem"}}>
+            <TextField
+              label="firstName"
+              variant="outlined"
+              name="firstName"
+              value={consumerFormData?.firstName}
+              onChange={handleFormChange}
+            />
+            <TextField
+              label="lastName"
+              variant="outlined"
+              name="lastName"
+              value={consumerFormData?.lastName}
+              onChange={handleFormChange}
+            />
+             <TextField
+              label="email"
+              variant="outlined"
+              name="emailId"
+              value={consumerFormData?.emailId}
+              onChange={handleFormChange}
+            />
+            <TextField
+              label="Contact"
+              variant="outlined"
+              name="mobileNumber"
+              value={consumerFormData?.mobileNumber}
+              onChange={handleFormChange}
+            />
+            <Button variant="contained" type="submit">
+              Save
+            </Button>
+          </form>
+
+        </Box>
+         
+      </Modal>
+    </div>
   );
 };
 
